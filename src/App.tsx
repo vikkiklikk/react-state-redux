71e195011c15e8197423fc1a5c1553bbbfd892d7
viktorbirgiss <viktor.birgisson@siminn.is>
2024-07-11 15:46:22 +0000

    init files

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+import Counter from "./components.tsx/Counter";
+
+// Store
+
+// Actions
+
+function App() {
+  return (
+    <>
+      <div className="text-xl">Redux</div>
+      <Counter />
+    </>
+  );
+}
+
+export default App;
